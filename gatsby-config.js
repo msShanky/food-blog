4109d52f280c3272ced9155374fd5228890a598a
shankyms <narayanan.s@pg.com>
2018-08-29 14:28:24 +0530

    Added Contenful Source Plugin and configured api key

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `1v1tt1rhrx9m`,
+        accessToken: `58ae76c44b515f676f13c8609ea709cabd629f7319b64036fa6b1b0c2440c07e`,      
+      },
+    },
+    'gatsby-plugin-react-helmet'
+  ],
 }
